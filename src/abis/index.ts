d0716435e6eef271b6932bcb6da91978b2a3bda5
Michael <37483994+neuroborus@users.noreply.github.com>
2025-02-10 10:09:59 +0100

    change abi file name to conventional

--- src/abis/index.ts
+++ src/abis/index.ts
@@ -1,5 +1,5 @@
 import ERC20_ABI from './erc-20.abi.json';
-import { MULTICALL_ABI } from './ethers.abi.multicall';
+import { MULTICALL_ABI } from './multicall.abi';
 
 export const erc20Abi = ERC20_ABI;
 export const multicallAbi = MULTICALL_ABI;
